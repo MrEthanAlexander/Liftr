3b077364050320283e25b74973234af9803c2e6e
tienn1998 <tienn@email.sc.edu>
2019-06-07 21:36:33 -0400

    partial user class

--- UserFile/User.java
+++ UserFile/User.java
@@ -8,6 +8,96 @@ public class User
   private boolean privacy;
   private int age;
   private String gender;
-  private double weight;
-  private
+  private int weight;
+  private int height;
+  private String bodyType;
+  private String description;
+  public User()
+  {
+    this.name = "no name";
+    this.username = "no username";
+    this.password = "no password";
+    this.securityQuestion = "no question";
+    this.securityAnswer = "no answer";
+    this.privacy = true;
+    this.age = 14;
+    this.gender = "no gender";
+    this.weight = 85.0;
+    this.height = 100.0;
+    this.bodyType = "no body type";
+    this.description = "empty description";
+  }
+  public User(String xName, String xUser, String xPass, String xQuestion, String xAnswer,
+  boolean xPrivacy, int xAge, String xGender, double xWeight, double xHeight, String xBody, String Description)
+  {
+    //TODO
+  }
+  public String getName()
+  {
+    return this.name;
+  }
+  public void setName(String name)
+  {
+    this.name = name;
+  }
+  public String getUser()
+  {
+    return this.username;
+  }
+  public void setUser(String user)
+  {
+    this.username = user;
+  }
+  public String getPassword()
+  {
+    return this.getPassword;
+  }
+  public void setPass(String pass)
+  {
+    this.password = pass;
+  }
+  public String getQuestion()
+  {
+    return this.securityQuestion;
+  }
+  public void setQuestion(String question)
+  {
+    this.securityQuestion = question;
+  }
+  public String getAnswer()
+  {
+    return this.securityAnswer;
+  }
+  public void setAnswer(String answer)
+  {
+    this.securityAnswer = answer;
+  }
+  public boolean getPrivacy()
+  {
+    return this.privacy;
+  }
+  public void setPrivacy(boolean privacy)
+  {
+    this.privacy = ;
+  }
+  public int getAge()
+  {
+    return this.age;
+  }
+  public int getWeight()
+  {
+    return this.weight;
+  }
+  public int getHeight()
+  {
+    return this.username;
+  }
+  public String getBody()
+  {
+    return this.bodyType;
+  }
+  public String getDescription()
+  {
+    return this.description;
+  }
 }
