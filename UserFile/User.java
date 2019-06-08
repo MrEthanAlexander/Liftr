65e3e9ca66e175a7363124dac7d7c7c10969f09c
tienn1998 <tienn@email.sc.edu>
2019-06-07 20:29:24 -0400

    user file

---
+++ UserFile/User.java
@@ -0,0 +1,13 @@
+public class User
+{
+  private String name;
+  private String username;
+  private String password;
+  private String securityQuestion;
+  private String securityAnswer;
+  private boolean privacy;
+  private int age;
+  private String gender;
+  private double weight;
+  private
+}
